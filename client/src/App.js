e502d26751669312c2d8c8ceafb95ab9720937c8
MaximKorolyov <max_klasiklmail.ru>
2024-02-20 00:52:05 +0300

    DevV1

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
+import './list_generator'
+import list_generator from './list_generator';
 
+const meowG = [{"task":"MEOWMEOW", "description":"HEHEHEHHE", "price":"50"}, {"task":"wowowowow", "description":"kruto", "price":"90909"}, {"task":"wowowowow", "description":"PF[OSDFP[ASO", "price":"90909"}] 
+
+
+
+
+// fetch('https://api.github.com/users/hacktivist123/repos')
 function App() {
+  const [contentt, setContent] = useState([{'title': 'загрузка...'}]);
+  useEffect(() => {
+    async function getAPI(){
+      let api = await fetch('http://localhost:4000/api');
+      let content = await api.json();
+      setContent(content)
+      return 1
+    }
+    getAPI();
+  }, [])
+
+  console.log(contentt[0])
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='main_div'>
+      {list_generator(contentt)}
     </div>
-  );
+    );
 }
 
 export default App;
